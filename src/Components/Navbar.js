289b5f06c0d407d40939c4bd46a08ce1b5fecb5e
yeeunsong <yeeunsong1019@gmail.com>
2021-09-17 19:34:32 +0900

    added navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Button } from "./Button";
+
+import "./Navbar.css";
+
+import Github from "../Assets/github.png";
+import LogoLong from "../Assets/logo-long.png";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <nav classNAme="navbar">
+        <div className="navbar-container">
+          <Link to="/" classNAme="navbar-logo">
+            <img src={LogoLong} className="div-header-logo" />
+          </Link>
+          {/* <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            TRVL
+            <i class="fab fa-typo3" />
+          </Link> */}
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/projects"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/experiences"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Experiences
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import "./home.css";
+import Header from '../Components/Header/Header'
+
+function Home() {
+  return (
+    <div className="Home">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
