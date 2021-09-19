c7f5ed97e0e876808a7dc93ad35c8372088de967
yeeunsong <yeeunsong1019@gmail.com>
2021-09-19 16:42:14 +0900

    added images to each side of threejs cube

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "react-three-fiber";
 import { OrbitControls, Stars } from "drei";
+import { TextureLoader } from "three";
 import "./Projects.css";
 
 function Box() {
+  const textureLoader = new TextureLoader();
+  const texture1 = textureLoader.load("/images/img-1.jpg");
+  const texture2 = textureLoader.load("/images/img-2.jpg");
+  const texture3 = textureLoader.load("/images/img-3.jpg");
+  const texture4 = textureLoader.load("/images/img-4.jpg");
+  const texture5 = textureLoader.load("/images/img-5.jpg");
+  const texture6 = textureLoader.load("/images/img-6.jpg");
+
   return (
     <mesh>
       <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="hotpink" />
+      <meshStandardMaterial attachArray="material" map={texture1} />
+      <meshStandardMaterial attachArray="material" map={texture2} />
+      <meshStandardMaterial attachArray="material" map={texture3} />
+      <meshStandardMaterial attachArray="material" map={texture4} />
+      <meshStandardMaterial attachArray="material" map={texture5} />
+      <meshStandardMaterial attachArray="material" map={texture6} />
     </mesh>
   );
 }
@@ -20,7 +34,9 @@ export default function Projects() {
         <Stars />
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 15, 10]} angle={0.3} />
-        <Box />
+        <Suspense fallback={null}>
+          <Box />
+        </Suspense>
       </Canvas>
     </div>
   );
