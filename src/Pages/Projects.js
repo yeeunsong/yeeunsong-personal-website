a5f690581a403281a2bab6a1b55354173d93619f
yeeunsong <yeeunsong1019@gmail.com>
2021-09-21 01:11:09 +0900

    added textGeometry

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -2,8 +2,27 @@ import React, { Suspense, useRef, useState, useEffect } from "react";
 import { Canvas, useFrame } from "react-three-fiber";
 import { OrbitControls, Stars } from "drei";
 import { TextureLoader } from "three";
+import * as THREE from "three";
 import { useSpring, a } from "react-spring/three";
 import "./Projects.css";
+import Montserrat_Bold from "../Assets/fonts/Montserrat_Bold.json";
+
+function Text({ position, size, height, text }) {
+  const font = new THREE.FontLoader().parse(Montserrat_Bold);
+
+  const textOptions = {
+    font,
+    size: size,
+    height: height,
+  };
+
+  return (
+    <mesh position={position}>
+      <textGeometry attach="geometry" args={[text, textOptions]} />
+      <meshStandardMaterial attach="material" />
+    </mesh>
+  );
+}
 
 function Sphere({ position, imageurl, link }) {
   const ref = useRef();
@@ -163,33 +182,45 @@ export default function Projects() {
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 15, 10]} angle={0.3} />
         <Suspense fallback={null}>
+          <Text
+            position={[-3, 2, -2]}
+            size={0.5}
+            height={0.1}
+            text={"Rotate the space around :)"}
+          />
+          <Text
+            position={[-5, -2, -3]}
+            size={0.5}
+            height={0.3}
+            text={"Click each planet"}
+          />
           <Sphere
-            position={[-2, 1, -5]}
+            position={[-2, 0, -5]}
             imageurl={"/images/img-1.jpg"}
             link={"https://github.com/yeeunsong/tab-android-app"}
           />
           <Torus
-            position={[5, 1, -2]}
+            position={[5, 0, -2]}
             imageurl={"/images/img-2.jpg"}
             link={"https://github.com/yeeunsong/DATATON_Recommendation_System"}
           />
           <Sphere
-            position={[0, 1, -2.5]}
+            position={[0, 0, -2.5]}
             imageurl={"/images/img-3.jpg"}
             link={"https://github.com/yeeunsong/SinGAN"}
           />
           <Sphere
-            position={[1, -1, 0]}
+            position={[1, -2, 0]}
             imageurl={"/images/img-4.jpg"}
             link={"https://github.com/yeeunsong/state-of-the-art-server"}
           />
           <Sphere
-            position={[-3, 2, -1]}
+            position={[-3, 1, -1]}
             imageurl={"/images/img-5.jpg"}
             link={"https://github.com/KU-BIG/nipa-connection-guide/wiki"}
           />
           <TorusKnot
-            position={[-4, -2, -5]}
+            position={[-4, -3, -5]}
             imageurl={"/images/img-6.jpg"}
             link={"https://github.com/yeeunsong/2019_Spring_novelty_detection"}
           />
