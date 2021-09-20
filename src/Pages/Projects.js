c2a76b1b5beaca35f2152882879ae2cbf65caed4
yeeunsong <yeeunsong1019@gmail.com>
2021-09-21 00:01:47 +0900

    rotation of spheres

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -5,21 +5,18 @@ import { TextureLoader } from "three";
 import { useSpring, a } from "react-spring/three";
 import "./Projects.css";
 
-function Box({ position, imageurl, link }) {
+function Sphere({ position, imageurl, link }) {
+  const ref = useRef();
   const textureLoader = new TextureLoader();
   const texture = textureLoader.load(imageurl);
-  const mesh = useRef();
-
-  //useFrame allows us to re-render/update rotation on each frame
-  // useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
   const [expand, setExpand] = useState(false);
+  const [hovered, setHovered] = useState(false);
+
+  useFrame((state, delta) => (ref.current.rotation.y += 0.01));
   const props = useSpring({
     scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
   });
-
-  const [hovered, setHovered] = useState(false);
-
   useEffect(() => {
     document.body.style.cursor = hovered ? "pointer" : "auto";
   }, [hovered]);
@@ -27,10 +24,17 @@ function Box({ position, imageurl, link }) {
   return (
     <a.mesh
       position={position}
-      onClick={(event) => window.open(link)}
-      onPointerOver={() => setHovered(true)}
-      onPointerOut={() => setHovered(false)}
+      onClick={() => window.open(link)}
+      onPointerOver={() => {
+        setHovered(true);
+        setExpand(!expand);
+      }}
+      onPointerOut={() => {
+        setHovered(false);
+        setExpand(!expand);
+      }}
       scale={props.scale}
+      ref={ref}
     >
       <sphereGeometry attach="geometry" args={[1, 32, 32]} />
       <meshStandardMaterial attach="material" map={texture} />
@@ -47,35 +51,35 @@ export default function Projects() {
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 15, 10]} angle={0.3} />
         <Suspense fallback={null}>
-          <Box
+          <Sphere
             position={[-2, 1, -5]}
             imageurl={"/images/img-1.jpg"}
-            link={"https://github.com/yeeunsong/state-of-the-art-server"}
+            link={"https://github.com/yeeunsong/tab-android-app"}
           />
-          <Box
+          <Sphere
             position={[5, 1, -2]}
             imageurl={"/images/img-2.jpg"}
             link={"https://github.com/yeeunsong/DATATON_Recommendation_System"}
           />
-          <Box
-            position={[0, 1, 0]}
+          <Sphere
+            position={[0, 1, -2.5]}
             imageurl={"/images/img-3.jpg"}
-            link={"https://github.com/yeeunsong/DATATON_Recommendation_System"}
+            link={"https://github.com/yeeunsong/SinGAN"}
           />
-          <Box
+          <Sphere
             position={[1, -1, 0]}
             imageurl={"/images/img-4.jpg"}
-            link={"https://github.com/yeeunsong/DATATON_Recommendation_System"}
+            link={"https://github.com/yeeunsong/state-of-the-art-server"}
           />
-          <Box
+          <Sphere
             position={[-3, 2, -1]}
             imageurl={"/images/img-5.jpg"}
-            link={"https://github.com/yeeunsong/DATATON_Recommendation_System"}
+            link={"https://github.com/KU-BIG/nipa-connection-guide/wiki"}
           />
-          <Box
-            position={[-4, -2, -2]}
+          <Sphere
+            position={[-4, -2, -5]}
             imageurl={"/images/img-6.jpg"}
-            link={"https://github.com/yeeunsong/DATATON_Recommendation_System"}
+            link={"https://github.com/yeeunsong/2019_Spring_novelty_detection"}
           />
         </Suspense>
       </Canvas>
